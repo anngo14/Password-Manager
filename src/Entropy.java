91abc0d2c3a407767eeaeb9542f51decca163daf
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-01-05 01:06:02 -0600

    basic setup for objects

--- src/Entropy.java
+++ src/Entropy.java
@@ -1,4 +1,26 @@
 
 public class Entropy {
+	
+	double bits;
 
+	public Entropy()
+	{
+		bits = 0.0;
+	}
+	
+	public Entropy(String password)
+	{
+		bits = calculateBits(password);
+	}
+	
+	public double getBits()
+	{
+		return this.bits;
+	}
+	
+	public double calculateBits(String password)
+	{
+		double entropyBits = 0.0;
+		return entropyBits;
+	}
 }
--- src/Password.java
+++ src/Password.java
@@ -1,4 +1,58 @@
 
 public class Password {
 
+	Entropy entropy;
+	String randomPassword;
+	String label;
+	
+	public Password()
+	{
+		this.label = "unknown";
+		this.randomPassword = generatePassword();
+		this.entropy = new Entropy(randomPassword);
+	}
+	public Password(String label)
+	{
+		this.label = label;
+		this.randomPassword = generatePassword();
+		this.entropy = new Entropy(randomPassword);
+	}
+	
+	public Password(String label, String password)
+	{
+		this.label = label;
+		this.randomPassword = password;
+		this.entropy = new Entropy(randomPassword);
+	}
+	
+	public double getEntropy()
+	{
+		return this.entropy.getBits();
+	}
+	
+	public String getRandomPassword()
+	{
+		return this.randomPassword;
+	}
+	
+	public String getLabel()
+	{
+		return this.label;
+	}
+	
+	public void setRandomPassword(String password)
+	{
+		this.randomPassword = password;
+	}
+	
+	public void setLabel(String label)
+	{
+		this.label = label;
+	}
+	
+	public String generatePassword()
+	{
+		String random = "";
+		return random;
+	}
 }
