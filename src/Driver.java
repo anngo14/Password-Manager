30183b4209198eedd799676941e049a9cc01ee18
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-01-07 16:21:46 -0600

    Basic implementation

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = "";
+		
+		while(!input.equals("START"))
+		{
+			System.out.print("Enter START to begin: ");
+			input = scanner.next();
+			input = input.toUpperCase();
+		}
+		while(!input.equals("END"))
+		{
+			System.out.print("Waiting for a command: ");
+			input = scanner.next();
+			input = input.toUpperCase();
+			switch(input)
+			{
+				case "GENERATE":
+					Password generator = new Password();
+					String random = generator.getRandomPassword();
+					Entropy check = new Entropy(random);
+
+					System.out.println("Generated Password: " + random);
+					System.out.println("Entropy of Generated Password: " + check.getBits());
+					break;
+				case "CHECK":
+					System.out.print("Enter a password to check Entropy: ");
+					input = scanner.next();
+					Entropy checker = new Entropy(input);
+					System.out.println("Entropy of Password: " + checker.getBits());
+					break;
+				default:
+					break;
+			}
+		}
+		System.out.println("Program Terminated");
+		
 
 	}
 
