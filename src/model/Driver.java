d05560d7bdab6c3563028ef7aa8ba61b5da4b3d3
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-04-04 14:38:42 -0500

    Icon change

--- src/model/Driver.java
+++ src/model/Driver.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -19,10 +20,12 @@ public class Driver extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		try
 		{
+			
 			Parent root=FXMLLoader.load(getClass().getResource("/view/Login Page.fxml"));
 			Scene scene = new Scene(root, 1000, 800);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Password Manager");
+			primaryStage.getIcons().add(new Image("Resources/09_lock-512.png"));
 			primaryStage.show();
 		} catch(Exception e)
 		{
