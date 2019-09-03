58cf1f2682d4d04e51d6a5c7eb86c7d9a290eac6
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-09-03 04:31:04 -0500

    Refactored postgre sql code

--- src/dao/DAOUtilities.java
+++ src/dao/DAOUtilities.java
@@ -4,32 +4,42 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 public class DAOUtilities {
 
 	/*private static final String CONNECTION_USERNAME = "postgres";
 	private static final String CONNECTION_PASSWORD = "password";
 	private static final String URL = "jdbc:postgresql://localhost:5432/passwordManager";*/
-	private static final String CONNECTION_USERNAME = "admin";
-	private static final String CONNECTION_PASSWORD = "Powermacg5";
-	private static final String URL = "jdbc:mysql://totemdb-1.cykewri9xfhv.us-east-2.rds.amazonaws.com/passwordManager";
+	private static final String DB_USER = "admin";
+	private static final String DB_PASSWORD = "Powermacg5";
+	private static final String DB_URL = "jdbc:mysql://totemdb-1.cykewri9xfhv.us-east-2.rds.amazonaws.com/passwordManager";
 	private static Connection connection;
 
 	static synchronized Connection getConnection() throws SQLException {
 		if (connection == null) {
-			try {
+			/*try {
 				Class.forName("org.postgresql.Driver");
 			} catch (ClassNotFoundException e) {
 				System.out.println("Could not register driver!");
 				e.printStackTrace();
-			}
-			connection = DriverManager.getConnection(URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
+			}*/
+			 connection = establishDataSource().getConnection();
 		}
 		
 		//If connection was closed then retrieve a new connection
 		if (connection.isClosed()){
 			System.out.println("getting new connection...");
-			connection = DriverManager.getConnection(URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
+			 connection = establishDataSource().getConnection();
 		}
 		return connection;
+	}
+	public static MysqlDataSource establishDataSource()
+	{
+		MysqlDataSource ds = new MysqlDataSource();
+		ds.setURL(DB_URL);
+		ds.setUser(DB_USER);
+		ds.setPassword(DB_PASSWORD);
+		return ds;
 	}
 }
--- src/model/Driver.java
+++ src/model/Driver.java
@@ -1,12 +1,10 @@
 package model;
-import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Driver extends Application{
